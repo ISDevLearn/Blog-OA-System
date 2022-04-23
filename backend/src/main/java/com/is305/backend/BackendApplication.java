2704f4f0b2226d52a02cda6ae9ba2d2595ff7d63
颜琪滨 <2535367985@qq.com>
2022-04-23 16:19:04 +0800

    Revert "框架测试"
    This reverts commit 5b1ca196a3e331429487d44ddb78b8cf0c28af89.

--- backend/src/main/java/com/is305/backend/BackendApplication.java
+++ backend/src/main/java/com/is305/backend/BackendApplication.java
@@ -2,8 +2,9 @@ package com.is305.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
 public class BackendApplication {
 
     public static void main(String[] args) {
