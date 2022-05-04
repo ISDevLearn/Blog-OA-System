70da08480ae7f6f23c5a21fafee9aaf2a75cc639
颜琪滨 <2535367985@qq.com>
2022-05-04 22:09:40 +0800

    using swagger to test api

--- backend/src/main/java/com/is305/backend/BackendApplication.java
+++ backend/src/main/java/com/is305/backend/BackendApplication.java
@@ -2,7 +2,11 @@ package com.is305.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
+@EnableWebMvc
 @SpringBootApplication
 public class BackendApplication {
 
