5f6bc799b568c9579b3f38ec779bf550124c70ae
颜琪滨 <2535367985@qq.com>
2022-05-15 19:09:30 +0800

    Revert "Merge branch 'yqb' of https://github.com/ISDevLearn/Blog-OA-System into yqb"
    This reverts commit abc53e0a47a48634105e0261538ca27ad889672f, reversing
    changes made to 544e36667d4db20128634ba2feecb4d9e0de87ed.

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,5 +1,4 @@
 import { Form, Input, Button, Checkbox } from 'antd';
-import React from 'react';
 import {UserOutlined, LockOutlined} from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import '../css/login.css'
