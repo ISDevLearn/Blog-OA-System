9641bdf465cfaee423f00a4cc369ee8875d6a576
A1paca233 <1667415927@qq.com>
2022-05-17 01:27:19 +0800

    主页暂存，现在比较寄

---
+++ frontend/src/components/home/header/left/HeaderLeft.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import { Dropdown, Menu, Input, message, Icon } from 'antd'
+import { Link } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
+
+import navList from '../right/NavList'
+
+// icon
+// import SvgIcon from '@/components/SvgIcon'
+import { MenuOutlined, SearchOutlined } from '@ant-design/icons'
+
+export const HEADER_BLOG_NAME = 'Test'
+
+const HeaderLeft = props => {
+    const [keyword, setKeyword] = useState('')
+    const history = useHistory()
+
+    function handleChange(e) {
+        e.preventDefault()
+        setKeyword(e.target.value)
+    }
+
+    function onPressEnter(e) {
+        e.target.blur()
+    }
+
+    function onSubmit() {
+        history.push(`/home?page=1&keyword=${keyword}`)
+        setKeyword('')
+    }
+
+    function clickSearch(e) {
+        e.stopPropagation()
+    }
+
+    const menu = (
+        <Menu className='header-nav'>
+            {navList.map(nav => (
+                <Menu.Item key={nav.link}>
+                    <Link to={nav.link}>
+                        {nav.icon}
+                        <span className='nav-text'>{nav.title}</span>
+                    </Link>
+                </Menu.Item>
+            ))}
+            <Menu.Item key={'search'}>
+                <SearchOutlined />
+                <Input
+                    className='search-input'
+                    onClick={clickSearch}
+                    value={keyword}
+                    onChange={handleChange}
+                    onPressEnter={onPressEnter}
+                    onBlur={onSubmit}
+                />
+            </Menu.Item>
+        </Menu>
+    )
+
+    return (
+        <div className='header-left'>
+            {/*<SvgIcon type='iconblog' style={{ color: '#055796', width: 16, height: 16, transform: 'translateY(-2px)' }} />*/}
+            <span className='blog-name' onClick={e => history.push('/home')} >{HEADER_BLOG_NAME}</span>
+            <Dropdown
+                overlayClassName='header-dropdown'
+                trigger={['click']}
+                overlay={menu}
+                getPopupContainer={() => document.querySelector('.app-header .header-left')}>
+                <MenuOutlined className='header-dropdown-icon' />
+            </Dropdown>
+        </div>
+    )
+}
+
+export default HeaderLeft
