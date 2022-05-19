19a610bdfc8949b98b5c7b5179d3b4b3ecc00686
颜琪滨 <2535367985@qq.com>
2022-05-19 10:15:04 +0800

    small change

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -109,7 +109,8 @@ const LoginForm = () => {
                 </Form.Item>
 
                 {/*TODO: 这里后面要加上跳转到忘记密码页面的路由*/}
-                <a className="login-form-forgot" onClick={history.push('/')}>
+                {/*<a className="login-form-forgot" onClick={history.push('/')}>*/}
+                <a>
                     忘记密码
                 </a>
             </Form.Item>
