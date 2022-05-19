7f546defd9dde9abda5d777f891c2c006b8cb97d
sgjzfzzf <sgjzfzzf@sjtu.edu.cn>
2022-05-19 18:21:23 +0800

    Add cors mappings into the config.

--- backend/src/main/java/com/is305/backend/config/WebConfig.java
+++ backend/src/main/java/com/is305/backend/config/WebConfig.java
@@ -3,13 +3,14 @@ package com.is305.backend.config;
 import com.is305.backend.Interceptor.LoginStatusInterceptor;
 import com.is305.backend.Interceptor.UsernameInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 
 @Configuration
-public class InterceptorConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Resource
     LoginStatusInterceptor loginStatusInterceptor;
@@ -23,4 +24,11 @@ public class InterceptorConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginStatusInterceptor).addPathPatterns("/user/**").addPathPatterns("/follower/**").addPathPatterns("/blog/**");
         registry.addInterceptor(usernameInterceptor).addPathPatterns("/follower/**");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**").allowedOriginPatterns("*").allowedHeaders("*").allowedHeaders("*").allowCredentials(true).maxAge(10000);
+    }
+
 }
